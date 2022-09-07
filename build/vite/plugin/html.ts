44a478ef9e04b7bbca81f4dfbef5a2341f1aee88
NonHa <yc337620547@163.com>
2022-09-07 10:02:44 +0800

    mare git ssh

--- build/vite/plugin/html.ts
+++ build/vite/plugin/html.ts
@@ -6,7 +6,6 @@ import type { PluginOption } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import pkg from '../../../package.json';
 import { GLOB_CONFIG_FILE_NAME } from '../../constant';
-
 export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
   const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
 
